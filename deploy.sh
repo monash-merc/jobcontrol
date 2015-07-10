625537b72d30a6301ea616d598d4741e71300c7b
jrigby <jason.rigby@monash.edu>
2015-07-10 12:59:08 +1000

    deployment script added

---
+++ deploy.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+cd $DIR
+mvn clean && mvn package
+scp target/strudel-web.war debian@autht.massive.org.au:~/
+ssh debian@autht.massive.org.au << EOF
+sudo rm /var/lib/tomcat7/webapps/strudel-web.war
+sudo rm -R /var/lib/tomcat7/webapps/strudel-web/
+sudo mv ~debian/strudel-web.war /var/lib/tomcat7/webapps/
+EOF
